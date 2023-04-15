cf3677b4ad991086f79d3704cb8ab7926694a6e4
Maylon-tech <minatokiroisen77@gmail.com>
2023-04-15 13:09:25 +0900

    setting the project and global theme

---
+++ src/GlobalComponents/ThemeProvider.js
@@ -0,0 +1,32 @@
+import React, {
+    createContext,
+    useState,
+    useEffect,
+    useContext
+} from 'react'
+
+const ThemeContext = createContext()
+
+const ThemeProvider = ( { children }) => {
+    const [theme, setTheme] = useState(
+        JSON.parse(localStorage.getItem('theme')) || false
+    )
+
+    useEffect(() => {
+        localStorage.setItem('theme', JSON.stringify(theme))
+    }, [theme])
+
+    const setThemeMode = mode => setTheme(mode)
+  return (
+    <ThemeContext.Provider value={{ theme, setThemeMode }}>
+        {children}
+    </ThemeContext.Provider>
+  )
+}
+
+const useThemeHook = () => {
+    const {theme} = useContext(ThemeContext)
+}
+
+
+export {ThemeProvider, ThemeContext}
